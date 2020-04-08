b50b0ac2ed88900bfff57b45fc0e5fc2f0c3ae28
chrisgauthier9 <17cpjg@queensu.ca>
2020-04-08 12:02:11 -0400

    Game Over Scene from solo mission added

---
+++ GameOverSceneFromSoloMisson.swift
@@ -0,0 +1,9 @@
+//
+//  GameOverSceneFromSoloMisson.swift
+//  2048
+//
+//  Created by Christophe Gauthier on 2020-04-08.
+//  Copyright © 2020 Christophe Gauthier. All rights reserved.
+//
+
+import Foundation
